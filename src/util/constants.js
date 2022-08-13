6e3e7677602ad77b83b859b3f086a5798a4b83ed
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-08-14 06:52:53 +0700

    Update constants.js

--- src/util/constants.js
+++ src/util/constants.js
@@ -2,5 +2,6 @@ module.exports.ENDPOINTS = {
   getUpdate: () => "getUpdates",
   sendMessage: () => "sendMessage",
   getMe: () => "getMe",
-  setWebhook: () => "setWebhook"
+  setWebhook: () => "setWebhook",
+  deleteWebhook: () => "deleteWebhook"
 }
