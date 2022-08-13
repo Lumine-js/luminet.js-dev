c3d5da06f9b88e13042e5510b1b05f1736294b7e
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-08-13 19:30:45 +0700

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -33,14 +33,16 @@ class Client extends EventEmitter {
     await this.requestAPI("GET", Constants.ENDPOINTS.getMe()).then(x => this.emit("ready", new UserClient(x.result)))
 
     await this.requestAPI("GET", Constants.ENDPOINTS.getUpdate()).then(denora => {
-      updates = denora.result.sort((a, b) => b.update_id - a.update_id)
-      if(updates.length > 0) {
+      if (denora.result.length > 0) {
+        updates = denora.result.sort((a, b) => b.update_id - a.update_id)
         latest = updates[0].update_id
       }
     })
     setInterval(async function() {
       await this.requestAPI("GET", Constants.ENDPOINTS.getUpdate()).then(denora => {
-        updates = denora.result.sort((a, b) => b.update_id - a.update_id)
+        if (denora.result.length > 0) {
+          updates = denora.result.sort((a, b) => b.update_id - a.update_id)
+        }
       })
       var newev = updates.filter(x => x.update_id > latest)
       if (newev.length > 0) {
@@ -64,9 +66,9 @@ class Client extends EventEmitter {
         "Content-type": "application/json"
       }
     }
-    
-    if(parameter) {
-      await parameter.forEach((x, u)=> {
+
+    if (parameter) {
+      await parameter.forEach((x, u) => {
         ccpn.url = ccpn.url + `${(u === 0) ? "?" : "&"}${x.title}=${encodeURIComponent(x.body)}`
       })
     }
@@ -80,8 +82,8 @@ class Client extends EventEmitter {
 
   sendMessage(channelId, content) {
     this.requestAPI("POST", Constants.ENDPOINTS.sendMessage(), [
-      { title:"chat_id",body: channelId},
-      { title:"text", body:content }])
+      { title: "chat_id", body: channelId },
+      { title: "text", body: content }])
   }
 }
 
